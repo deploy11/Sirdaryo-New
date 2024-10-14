0910cd733b96e2472c4be4807fc16c3b0cb18c92
deploy11 <129506514+deploy11@users.noreply.github.com>
2024-10-14 13:05:41 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,9 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = [
-    'https://sirdaryo-new-production.up.railway.app/',
-]
+CSRF_TRUSTED_ORIGINS = ['https://sirdaryo-new-production.up.railway.app/']
 
 # Application definition
 
